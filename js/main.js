fbac49f16f3d3d0933a9b272ee383bb8da3db13a
Olya <kod678@gmail.com>
2021-02-10 11:34:32 +0300

    10 feb

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 /* Маска для телефона */
 jQuery(function($) {
     $("#tel").mask("+7(999) - 999 - 99 - 99");
-    $("#modal-call-tel").mask("+7(999) - 999 - 99 - 99");
-    $("#modal-recording-tel").mask("+7(999) - 999 - 99 - 99");
-    $("#form-instructors-tel").mask("+7(999) - 999 - 99 - 99");
 });   
 /* Маска для телефона */
+
+$(document).ready(function(){
+    var $header = $("#header");     
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 120 && $header.hasClass("default") ){
+            $header.fadeOut('fast',function(){
+                $(this).removeClass("default")
+                        .addClass("fixed")
+                        .fadeIn('fast');
+            });
+        } else if($(this).scrollTop() <= 120 && $header.hasClass("fixed")) {
+            $header.fadeOut('fast',function(){
+                $(this).removeClass("fixed")
+                        .addClass("default")
+                        .fadeIn('fast');
+            });
+        }
+    });        
+});
 
