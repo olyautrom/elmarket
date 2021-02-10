ecebc4b22403433f26b08d8297c5e4cdefb07b9b
Olya <kod678@gmail.com>
2021-02-10 15:16:16 +0300

    10 feb

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ jQuery(function($) {
 /* Маска для телефона */
 
 $(document).ready(function(){
-    var $header = $("#header");     
+    var $header = $("#header");
     $(window).scroll(function(){
-        if ( $(this).scrollTop() > 120 && $header.hasClass("default") ){
+        if ( $(this).scrollTop() > 100 && $header.hasClass("default") ){
             $header.fadeOut('fast',function(){
                 $(this).removeClass("default")
                         .addClass("fixed")
                         .fadeIn('fast');
             });
-        } else if($(this).scrollTop() <= 120 && $header.hasClass("fixed")) {
-            $header.fadeOut('fast',function(){
+        } else if($(this).scrollTop() <= 100 && $header.hasClass("fixed")) {
+            $header.fadeOut('400',function(){
                 $(this).removeClass("fixed")
                         .addClass("default")
                         .fadeIn('fast');
